c509de40961ede26b0ba4ffb61080103207d969e
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-08-25 17:15:19 -0400

    test: expose internals to Remote Force runtime smoke

--- windows/AsterMax.MechanicalGui/AssemblyInfo.cs
+++ windows/AsterMax.MechanicalGui/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("AsterMax.MechanicalGui.DomainSmoke")]
+[assembly: InternalsVisibleTo("AsterMax.MechanicalGui.RemoteForceSmoke")]
