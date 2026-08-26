d5bf8b49aa29b530fb0b053e0b603c90917cbde5
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-08-26 18:49:06 -0400

    Export TRI6 pressure provenance gate

--- src/astermax/contact/__init__.py
+++ src/astermax/contact/__init__.py
@@ -6,6 +6,15 @@ from .tet10_unilateral import (
     Tet10SingleDofContactResult,
     solve_tet10_single_dof_unilateral_contact,
 )
+from .tri6_pressure import (
+    Tri6PressureRecoveryResult,
+    Tri6PressureRecoveryStatus,
+    recover_consistent_tri6_pressure,
+    triangle_area_mm2,
+    tri6_consistent_pressure_matrix_mm2,
+    tri6_pressure_value_mpa,
+    tri6_quadratic_pressure_extrema,
+)
 from .unilateral import (
     ContactState,
     UnilateralSpringContactProblem,
@@ -18,10 +27,17 @@ __all__ = [
     "ContactState",
     "Tet10MultipointContactResult",
     "Tet10SingleDofContactResult",
+    "Tri6PressureRecoveryResult",
+    "Tri6PressureRecoveryStatus",
     "UnilateralSpringContactProblem",
     "UnilateralSpringContactResult",
+    "recover_consistent_tri6_pressure",
     "solve_tet10_multipoint_unilateral_contact",
     "solve_tet10_single_dof_unilateral_contact",
     "solve_unilateral_spring_contact",
     "solve_unilateral_spring_contact_sweep",
+    "triangle_area_mm2",
+    "tri6_consistent_pressure_matrix_mm2",
+    "tri6_pressure_value_mpa",
+    "tri6_quadratic_pressure_extrema",
 ]
