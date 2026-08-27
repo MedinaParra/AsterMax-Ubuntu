8e0be9a03d01d294a04e5c3a2d957909d81388e3
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-08-27 05:31:10 -0400

    Export TRI6 surface pressure contact API

--- src/astermax/contact/__init__.py
+++ src/astermax/contact/__init__.py
@@ -15,6 +15,14 @@ from .tri6_pressure import (
     tri6_pressure_value_mpa,
     tri6_quadratic_pressure_extrema,
 )
+from .tri6_surface_contact import (
+    TRI6_GAUSS_BARYCENTRIC,
+    Tet10Tri6SurfacePressureContactResult,
+    solve_tet10_tri6_surface_pressure_contact,
+    tri6_shape_functions,
+    tri6_surface_operator,
+    tri6_surface_pressure_generalized_force,
+)
 from .unilateral import (
     ContactState,
     UnilateralSpringContactProblem,
@@ -25,8 +33,10 @@ from .unilateral import (
 
 __all__ = [
     "ContactState",
+    "TRI6_GAUSS_BARYCENTRIC",
     "Tet10MultipointContactResult",
     "Tet10SingleDofContactResult",
+    "Tet10Tri6SurfacePressureContactResult",
     "Tri6PressureRecoveryResult",
     "Tri6PressureRecoveryStatus",
     "UnilateralSpringContactProblem",
@@ -34,10 +44,14 @@ __all__ = [
     "recover_consistent_tri6_pressure",
     "solve_tet10_multipoint_unilateral_contact",
     "solve_tet10_single_dof_unilateral_contact",
+    "solve_tet10_tri6_surface_pressure_contact",
     "solve_unilateral_spring_contact",
     "solve_unilateral_spring_contact_sweep",
     "triangle_area_mm2",
     "tri6_consistent_pressure_matrix_mm2",
     "tri6_pressure_value_mpa",
     "tri6_quadratic_pressure_extrema",
+    "tri6_shape_functions",
+    "tri6_surface_operator",
+    "tri6_surface_pressure_generalized_force",
 ]
